e081e0ceb860a1788144f037da16fde9583719d4
Fahim shahoriar <mffahim8899@gmail.com>
2026-09-16 07:40:20 +0600

    Assingment 5 startup

---
+++ Dev -stack/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
